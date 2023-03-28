ff63ce7256d0601cbb5fb2704a7e4dc20b5254e7
feanor <umutmnalci@hotmail.com>
2023-03-28 17:52:32 +0300

    Add order model

---
+++ order.go
@@ -0,0 +1,26 @@
+package simuexos
+
+import "time"
+
+/*
+Order represents an order created by an account.
+Orders are placed into their corresponding orderbook and can either be cancalled or settled.
+Current implementation of the exchange will close an open a new order in response to an order modification request.
+Partial fills and related data will be filled on settlement logs while referencing the order, instead of in the order struct,
+therefore lack of data such as remaining amount is not kept here per the implementation details.
+*/
+type Order struct {
+	ID int
+
+	Creator Account
+
+	CreationDate time.Time
+	ExpiryDate   time.Time
+
+	Token     Token
+	BaseToken Token
+
+	// Price and Amount are currently typed as int, will be switched to proper types once implemented
+	Price  int
+	Amount int
+}
